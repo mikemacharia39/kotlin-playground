7ce7ea1a1c1194e83ec11cdf3a45c64e84520353
mikemacharia39 <mikemacharia39@gmail.com>
2023-02-19 22:13:25 +0300

    Define enumerations for address and employee

---
+++ src/main/kotlin/org/mikehenry/kotlin_playground/domain/enumeration/AddressType.kt
@@ -0,0 +1,7 @@
+package org.mikehenry.kotlin_playground.domain.enumeration
+
+enum class AddressType {
+    HOME,
+    WORK,
+    OTHER
+}
---
+++ src/main/kotlin/org/mikehenry/kotlin_playground/domain/enumeration/EmployeeStatus.kt
@@ -0,0 +1,8 @@
+package org.mikehenry.kotlin_playground.domain.enumeration
+
+enum class EmployeeStatus {
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED,
+    TERMINATED
+}
