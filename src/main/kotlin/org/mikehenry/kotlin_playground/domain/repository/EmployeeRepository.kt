f3319139d5d08b6d78789b5d776b14d2f73ff270
mikemacharia39 <mikemacharia39@gmail.com>
2023-02-21 23:08:39 +0300

    Fix repository and changelog path

---
+++ src/main/kotlin/org/mikehenry/kotlin_playground/domain/repository/EmployeeRepository.kt
@@ -0,0 +1,8 @@
+package org.mikehenry.kotlin_playground.domain.repository
+
+import org.mikehenry.kotlin_playground.domain.entity.Employee
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface EmployeeRepository: JpaRepository<Employee, Long>
