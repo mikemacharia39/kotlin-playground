30c8df379d85a6fd427239b1894ed6ffdcefb741
mikemacharia39 <mikemacharia39@gmail.com>
2023-02-23 00:34:40 +0300

    Update exception handling

--- src/main/kotlin/org/mikehenry/kotlin_playground/domain/exception/ExceptionControllerAdvise.kt
+++ src/main/kotlin/org/mikehenry/kotlin_playground/domain/exception/ExceptionControllerAdvise.kt
@@ -1,6 +1,8 @@
 package org.mikehenry.kotlin_playground.domain.exception
 
 import org.mikehenry.kotlin_playground.api.dto.request.RequestErrors
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -10,18 +12,18 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 class ExceptionControllerAdvise: ResponseEntityExceptionHandler() {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @Override
     override fun handleMethodArgumentNotValid(ex: MethodArgumentNotValidException, headers: HttpHeaders,
                                               status: HttpStatus, request: WebRequest): ResponseEntity<Any> {
         val requestErrors = RequestErrors(
             type = "https://mikehenry-maina.com/problems/kotlin-playgroung/constraint-violation",
             title = ex.bindingResult.allErrors.first().defaultMessage,
             status = HttpStatus.BAD_REQUEST,
-            message = "error.http.400",
+            message = ex.message,
             params = ex.bindingResult.allErrors.first().objectName
         )
         return ResponseEntity(requestErrors, HttpStatus.BAD_REQUEST)
