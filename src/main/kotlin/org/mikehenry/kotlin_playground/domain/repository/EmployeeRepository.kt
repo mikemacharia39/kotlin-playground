ad71c10ae7ae8c220e2c036153343c4121434805
mikemacharia39 <mikemacharia39@gmail.com>
2023-08-30 04:26:15 +0300

    some minor fix

--- src/main/kotlin/org/mikehenry/kotlin_playground/domain/repository/EmployeeRepository.kt
+++ src/main/kotlin/org/mikehenry/kotlin_playground/domain/repository/EmployeeRepository.kt
@@ -2,7 +2,9 @@ package org.mikehenry.kotlin_playground.domain.repository
 
 import org.mikehenry.kotlin_playground.domain.entity.Employee
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
-interface EmployeeRepository: JpaRepository<Employee, Long>
+interface EmployeeRepository: JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
+}
