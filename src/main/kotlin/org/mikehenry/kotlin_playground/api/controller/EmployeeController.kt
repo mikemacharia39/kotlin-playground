89c01edbabdc24ad6fe6c02a82bc5d16c659f261
mikemacharia39 <mikemacharia39@gmail.com>
2024-02-25 22:37:15 +0300

    search employee feature

--- src/main/kotlin/org/mikehenry/kotlin_playground/api/controller/EmployeeController.kt
+++ src/main/kotlin/org/mikehenry/kotlin_playground/api/controller/EmployeeController.kt
@@ -13,12 +13,15 @@ import org.mikehenry.kotlin_playground.api.dto.request.RequestErrors
 import org.mikehenry.kotlin_playground.api.dto.response.EmployeeResponseDto
 import org.mikehenry.kotlin_playground.domain.service.EmployeeBulkUploadService
 import org.mikehenry.kotlin_playground.domain.service.EmployeeService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
@@ -31,8 +34,10 @@ class EmployeeController(
     private val employeeBulkUploadService: EmployeeBulkUploadService
 ) {
 
-    @PostMapping(consumes = ["application/json"],
-                produces = ["application/json", "application/problem+json"])
+    @PostMapping(
+        consumes = ["application/json"],
+        produces = ["application/json", "application/problem+json"]
+    )
     @ApiOperation("Adds a new employee record")
     @ApiResponses(
         ApiResponse(code = SC_ACCEPTED, message = "Accepted.\n Successfully added employee record"),
@@ -53,7 +58,8 @@ class EmployeeController(
             response = RequestErrors::class
         )
     )
-    fun addEmployee(@Valid @RequestBody employeeRequestDto: EmployeeRequestDto): EmployeeResponseDto = employeeService.addEmployee(employeeRequestDto)
+    fun addEmployee(@Valid @RequestBody employeeRequestDto: EmployeeRequestDto): EmployeeResponseDto =
+        employeeService.addEmployee(employeeRequestDto)
 
 
     @PostMapping("/bulk", consumes = ["multipart/form-data"])
@@ -63,4 +69,17 @@ class EmployeeController(
 
     @GetMapping("/{employeeId}")
     fun getEmployee(@PathVariable employeeId: Long): EmployeeResponseDto = employeeService.getEmployee(employeeId)
+
+    @GetMapping("/all")
+    fun getAllEmployees(pageable: Pageable): Page<EmployeeResponseDto> = employeeService.getAllEmployees(pageable)
+
+    @GetMapping("/search")
+    fun searchEmployees(
+        @RequestParam("employeeName", required = false) employeeName: String?,
+        @RequestParam("employeeIds", required = false) employeeIds: List<Long>?,
+        @RequestParam("phoneNumber", required = false) phoneNumber: String?,
+        @RequestParam("departmentName", required = false) departmentName: String?,
+        @RequestParam("address", required = false) address: String?,
+        pageable: Pageable
+    ): Page<EmployeeResponseDto> = employeeService.searchEmployees(employeeName, employeeIds, phoneNumber, departmentName, address, pageable)
 }
--- src/main/kotlin/org/mikehenry/kotlin_playground/domain/service/EmployeeService.kt
+++ src/main/kotlin/org/mikehenry/kotlin_playground/domain/service/EmployeeService.kt
@@ -5,6 +5,9 @@ import org.mikehenry.kotlin_playground.api.dto.response.EmployeeResponseDto
 import org.mikehenry.kotlin_playground.domain.exception.NotFoundProblem
 import org.mikehenry.kotlin_playground.domain.mapper.EmployeeMapper
 import org.mikehenry.kotlin_playground.domain.repository.EmployeeRepository
+import org.mikehenry.kotlin_playground.domain.specification.searchEmployee
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -27,5 +30,23 @@ class EmployeeService(
             .orElseThrow { throw NotFoundProblem("error.employee.not.found", mapOf("employeeId" to employeeId)) }
 
         return employeeMapper.mapEntityToDTO(employee)
+    }
+
+    fun getAllEmployees(pageable: Pageable): Page<EmployeeResponseDto> {
+        val employees = employeeRepository.findAll(pageable)
+        return employees.map { employeeMapper.mapEntityToDTO(it) }
+    }
+
+    fun searchEmployees(
+        employeeName: String?,
+        employeeIds: List<Long>?,
+        phoneNumber: String?,
+        departmentName: String?,
+        address: String?,
+        pageable: Pageable
+    ): Page<EmployeeResponseDto> {
+        return employeeRepository.findAll(
+            searchEmployee(employeeName, employeeIds, phoneNumber, departmentName, address), pageable
+        ).map { employeeMapper.mapEntityToDTO(it) }
     }
 }
