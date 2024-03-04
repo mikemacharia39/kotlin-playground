c6b0151076c6d91e4a0aece15ef486c91e0197a6
mikemacharia39 <mikemacharia39@gmail.com>
2024-03-04 21:57:46 +0300

    presigner configuration

--- src/main/kotlin/org/mikehenry/kotlin_playground/configuration/S3ConfigurationToTest.kt
+++ src/main/kotlin/org/mikehenry/kotlin_playground/configuration/S3ConfigurationToTest.kt
@@ -11,6 +11,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 
@@ -27,6 +28,10 @@ class S3ConfigurationToTest {
     @Bean
     fun s3Client(): S3Client = awsS3Client()
 
+    @Profile("prod")
+    @Bean
+    fun s3Presigner(): S3Presigner = awsS3Presigner()
+
     companion object {
         @Container
         private val localstack = LocalStackContainer(DockerImageName.parse("localstack/localstack:0.11.3"))
@@ -34,12 +39,24 @@ class S3ConfigurationToTest {
             .withEnv("DEFAULT_REGION", "us-east-1")
             .withEnv("DEBUG", "1")
             .withEnv("TESTCONTAINERS_RYUK_DISABLED", "true")
+            .withReuse(true)
             .withStartupTimeout(Duration.of(2, ChronoUnit.MINUTES))
     }
 
     fun awsS3Client(): S3Client {
         log.info { "Starting LocalStack container on ${localstack.getEndpointOverride(LocalStackContainer.Service.S3)}" }
         return S3Client.builder()
+            .endpointOverride(localstack.getEndpointOverride(LocalStackContainer.Service.S3))
+            .credentialsProvider(
+                StaticCredentialsProvider.create(AwsBasicCredentials.create(localstack.accessKey, localstack.secretKey))
+            )
+            .region(Region.of(localstack.region))
+            .build()
+    }
+
+    fun awsS3Presigner(): S3Presigner {
+        log.info { "Starting LocalStack container on ${localstack.getEndpointOverride(LocalStackContainer.Service.S3)}" }
+        return S3Presigner.builder()
             .endpointOverride(localstack.getEndpointOverride(LocalStackContainer.Service.S3))
             .credentialsProvider(
                 StaticCredentialsProvider.create(AwsBasicCredentials.create(localstack.accessKey, localstack.secretKey))
