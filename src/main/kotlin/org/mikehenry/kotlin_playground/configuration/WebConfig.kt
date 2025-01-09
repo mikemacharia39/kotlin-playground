1e3dcd848ea95c27cc176dba553c5d348d9dda2f
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-09 17:51:15 +0300

    Using thymeleaf

---
+++ src/main/kotlin/org/mikehenry/kotlin_playground/configuration/WebConfig.kt
@@ -0,0 +1,21 @@
+package org.mikehenry.kotlin_playground.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig {
+
+    @Bean
+    public fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:6000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+            }
+        }
+    }
+}
