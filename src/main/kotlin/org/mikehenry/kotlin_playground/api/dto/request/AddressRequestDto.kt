ac4711e9143c0ad2c954940455ad2aa7fc75be48
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-09 21:04:47 +0300

    Functionality to view add delete using thymeleaf

--- src/main/kotlin/org/mikehenry/kotlin_playground/api/dto/request/AddressRequestDto.kt
+++ src/main/kotlin/org/mikehenry/kotlin_playground/api/dto/request/AddressRequestDto.kt
@@ -1,27 +1,33 @@
 package org.mikehenry.kotlin_playground.api.dto.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 import lombok.AllArgsConstructor
 import lombok.Builder
 import lombok.Getter
+import lombok.NoArgsConstructor
 import lombok.Setter
 import org.mikehenry.kotlin_playground.domain.enumeration.AddressType
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 class AddressRequestDto(
-
     val addressType: AddressType = AddressType.HOME,
     @NotBlank
-    val addressLine1: String,
+    @field:Size(min = 2, max = 200)
+    val addressLine1: String? = "",
     val addressLine2: String? = null,
     @NotBlank
-    val country: String,
+    @field:Size(min = 2)
+    val country: String? = "",
     @NotBlank
-    val city: String,
+    @field:Size(min = 2)
+    val city: String? = "",
     val state: String? = null,
     @NotBlank
-    val postalBox: String
+    @field:Size(min = 2)
+    val postalBox: String? = ""
 )
