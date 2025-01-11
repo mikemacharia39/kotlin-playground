eb5a0fab2f017acb9c80d0b28600611c09138508
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-11 23:11:10 +0300

    using openfeign

--- src/main/kotlin/org/mikehenry/kotlin_playground/domain/specification/EmployeeSpecification.kt
+++ src/main/kotlin/org/mikehenry/kotlin_playground/domain/specification/EmployeeSpecification.kt
@@ -3,48 +3,45 @@ package org.mikehenry.kotlin_playground.domain.specification
 import jakarta.persistence.criteria.Join
 import jakarta.persistence.criteria.JoinType
 import jakarta.persistence.criteria.Predicate
+import org.mikehenry.kotlin_playground.api.dto.params.EmployeeSearchParams
 import org.mikehenry.kotlin_playground.domain.entity.Address
 import org.mikehenry.kotlin_playground.domain.entity.Department
 import org.mikehenry.kotlin_playground.domain.entity.Employee
 import org.springframework.data.jpa.domain.Specification
 
 fun searchEmployee(
-    employeeName: String?,
-    employeeIds: List<Long>?,
-    phoneNumber: String?,
-    departmentName: String?,
-    address: String?,
+    employeeSearchParams: EmployeeSearchParams
 ): Specification<Employee> {
     return Specification { root, _, criteriaBuilder ->
         val predicates = mutableListOf<Predicate>()
-        if (employeeName != null) {
+        if (employeeSearchParams.employeeName != null) {
             predicates.add(
                 criteriaBuilder.or(
-                    criteriaBuilder.like(root.get("firstName"), "%$employeeName%"),
-                    criteriaBuilder.like(root.get("lastName"), "%$employeeName%")
+                    criteriaBuilder.like(root.get("firstName"), "%${employeeSearchParams.employeeName}%"),
+                    criteriaBuilder.like(root.get("lastName"), "%${employeeSearchParams.employeeName}%")
                 )
             )
         }
-        if (employeeIds != null) {
-            predicates.add(root.get<Long>("employeeId").`in`(employeeIds))
+        if (employeeSearchParams.employeeIds != null) {
+            predicates.add(root.get<Long>("employeeId").`in`(employeeSearchParams.employeeIds))
         }
-        if (phoneNumber != null) {
-            predicates.add(criteriaBuilder.like(root.get("phoneNumber"), "%$phoneNumber%"))
+        if (employeeSearchParams.phoneNumber != null) {
+            predicates.add(criteriaBuilder.like(root.get("phoneNumber"), "%${employeeSearchParams.phoneNumber}%"))
         }
-        if (departmentName != null) {
+        if (employeeSearchParams.departmentName != null) {
             val departmentJoin: Join<Employee, Department> = root.join("department", JoinType.INNER)
-            predicates.add(criteriaBuilder.like(departmentJoin.get("departmentName"), "%$departmentName%"))
+            predicates.add(criteriaBuilder.like(departmentJoin.get("departmentName"), "%${employeeSearchParams.departmentName}%"))
         }
-        if (address != null) {
+        if (employeeSearchParams.address != null) {
             val addressJoin: Join<Employee, Address> = root.join("addresses", JoinType.INNER)
             predicates.add(
                 criteriaBuilder.or(
-                    criteriaBuilder.like(addressJoin.get("addressLine1"), "%$address%"),
-                    criteriaBuilder.like(addressJoin.get("addressLine2"), "%$address%"),
-                    criteriaBuilder.like(addressJoin.get("city"), "%$address%"),
-                    criteriaBuilder.like(addressJoin.get("state"), "%$address%"),
-                    criteriaBuilder.like(addressJoin.get("country"), "%$address%"),
-                    criteriaBuilder.like(addressJoin.get("postalBox"), "%$address%")
+                    criteriaBuilder.like(addressJoin.get("addressLine1"), "%${employeeSearchParams.address}%"),
+                    criteriaBuilder.like(addressJoin.get("addressLine2"), "%${employeeSearchParams.address}%"),
+                    criteriaBuilder.like(addressJoin.get("city"), "%${employeeSearchParams.address}%"),
+                    criteriaBuilder.like(addressJoin.get("state"), "%${employeeSearchParams.address}%"),
+                    criteriaBuilder.like(addressJoin.get("country"), "%${employeeSearchParams.address}%"),
+                    criteriaBuilder.like(addressJoin.get("postalBox"), "%${employeeSearchParams.address}%")
                 )
             )
         }
