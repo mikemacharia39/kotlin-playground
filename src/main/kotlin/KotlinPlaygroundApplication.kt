854e62b512448326ffb89ab231df15365e50a0cb
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-13 14:57:26 +0300

    addition of enablefeignclients dependency

--- src/main/kotlin/KotlinPlaygroundApplication.kt
+++ src/main/kotlin/KotlinPlaygroundApplication.kt
@@ -1,6 +1,7 @@
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Profile
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
